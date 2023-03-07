8981c5961ce42fdee4b667663cef0a739c7c247f
VGrolleau <virginie.grolleau+github@protonmail.com>
2023-03-07 11:18:48 +0100

    Try import React differently to fix bug

--- src/components/DropdownMenu/DropdownMenu.jsx
+++ src/components/DropdownMenu/DropdownMenu.jsx
@@ -1,5 +1,5 @@
-import { PropTypes } from 'prop-types';
-import { React, useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import PropTypes from 'prop-types';
 import './DropdownMenu.css';
 
 function DropdownMenu(props) {
